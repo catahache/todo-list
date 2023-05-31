1ba23aeb791e9f6f28604abb32648dd0c7af0551
catahache <herediacatalina1995@gmail.com>
2023-05-31 17:54:22 -0300

    tasks modal

---
+++ src/views/todoList/EditModal.tsx
@@ -0,0 +1,93 @@
+import { Button, Checkbox, Form, Input, InputNumber, Modal, Select } from "antd"
+import { Task } from "../../interfaces/todoList"
+import { useDispatch, useSelector } from "react-redux"
+import { AppDispatch, RootState } from "../../store/store"
+import { createTask } from "../../store/slices/todoList.slice"
+import { useEffect, useState } from "react"
+import { getChosenTask } from "../../api/todo"
+
+const EditModal = ({ open, handleModal, id }) => { //TODO solucionar ts
+    const dispatch = useDispatch<AppDispatch>()
+    const { auth } = useSelector((state: RootState) => state.authData)
+    const [chosenTask, setChosenTask] = useState({}) //TODO ts
+    const [loading, setLoading] = useState(false) //TODO ts
+
+    useEffect(() => {
+        if (id) {
+            setLoading(true)
+            loadChosenTask()
+        }
+    }, [id])
+
+    const loadChosenTask = async () => {
+        const response = await getChosenTask(id)
+        setChosenTask(response.data)
+        setLoading(false)
+    }
+
+
+    const onFinish = (values: Task) => {
+        if (!!id) { //Edits task
+            console.log(`edit task ${id}`)
+        } else {
+            dispatch(createTask({ //Creates task
+                ...values,
+                user: auth.user._id,
+            }))
+        }
+        handleModal(false)
+    }
+
+    return (
+        <Modal
+            afterClose={() => setChosenTask({})}
+            title="Crear nueva tarea"
+            open={open}
+            confirmLoading={false}
+            onCancel={() => handleModal(false)}
+            footer={[
+                <Button form="createTask" key="submit" htmlType="submit">
+                    {!!id ? 'Editar' : 'Crear'}
+                </Button>
+            ]}
+            destroyOnClose={true}
+        >{!loading &&
+            <Form
+                id="createTask"
+                labelCol={{ span: 4 }}
+                wrapperCol={{ span: 14 }}
+                layout="horizontal"
+                style={{ maxWidth: 600 }}
+                onFinish={onFinish}
+                onValuesChange={v => console.log(v)}
+                initialValues={!!id ? { done: chosenTask.done, task: chosenTask.task } : {}}
+                preserve={false}
+            >
+                <Form.Item label="Terminada" name="done" valuePropName="checked">
+                    <Checkbox />
+                </Form.Item>
+                <Form.Item label="Tarea" name="task">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Tipo de tarea" name="type">
+                    <Select>
+                        <Select.Option value="study">Estudio</Select.Option>
+                        <Select.Option value="work">Trabajo</Select.Option>
+                        <Select.Option value="home">Hogar</Select.Option>
+                        <Select.Option value="others">Otras</Select.Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item label="Tareas relacionadas" name="subTasks">
+                    <Select mode="tags" placeholder="Agregar...">
+                        <Select.Option key={0}>Comprar manteca</Select.Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item label="Prioridad" name="priority">
+                    <InputNumber />
+                </Form.Item>
+            </Form>}
+        </Modal>
+    )
+}
+
+export default EditModal
