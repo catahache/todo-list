0bd09c8af104ef63319d22fa3de22d06e932c2f4
catahache <herediacatalina1995@gmail.com>
2023-05-31 17:53:30 -0300

    required auth component

---
+++ src/routes/RequireAuth.tsx
@@ -0,0 +1,16 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+
+
+const RequireAuth = () => {
+    const { auth } = useSelector((state: RootState) => state.authData);
+    const location = useLocation();
+    return auth.token ? (
+        <Outlet />
+    ) : (
+        <Navigate to="/unauthorized" state={{ from: location }} replace />
+    );
+};
+
+export default RequireAuth;
