a759ccde039bd9a3dfdec7b4c9614b58432ef983
catahache <herediacatalina1995@gmail.com>
2023-06-01 01:07:00 -0300

    change color

--- src/constants/tasks.ts
+++ src/constants/tasks.ts
@@ -1,5 +1,5 @@
 const TYPES = [
-    { type: 'study', label: 'Estudio', color: '#FF5722' },
+    { type: 'study', label: 'Estudio', color: '#4CAF50' },
     { type: 'work', label: 'Trabajo', color: '#448AFF' },
     { type: 'home', label: 'Hogar', color: '#9C27B0' },
     { type: 'others', label: 'Otras', color: '#455A64' },
